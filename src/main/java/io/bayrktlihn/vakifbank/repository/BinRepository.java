e80e57ef17bf35218137bbbe99ff9c3fc54ab35d
bayrktlihn <bayraktaralihan1995@gmail.com>
2023-08-12 20:53:37 +0300

    unmodifiable bin list

--- src/main/java/io/bayrktlihn/vakifbank/repository/BinRepository.java
+++ src/main/java/io/bayrktlihn/vakifbank/repository/BinRepository.java
@@ -14,6 +14,7 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Repository
@@ -60,7 +61,7 @@ public class BinRepository {
             }
 
 
-            this.binList = binList;
+            this.binList = Collections.unmodifiableList(binList);
 
 
         } catch (Exception e) {
